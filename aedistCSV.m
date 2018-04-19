1daf3add1e3443057036cd95eb5352f6e1248c80
kamilvlcek <kamil@biomed.cas.cz>
2018-04-19 15:47:06 +0200

    AEdist3D - analyza vice csv soucasne, ukladani obrazku na pozadi do png, zmena folderu pro vystupni adresare aj
    - cyklus pres soubory nacita postupne i ruzne csv soubory. Musi mi stejne obrazky.
    - vystupni soubory se ukladaji k datum, podle noveho parametru datafolder
    - vsechny obrazky subjektu se ukladaji do png souboru a uz ani nezobrazujou

--- aedistCSV.m
+++ aedistCSV.m
@@ -1,46 +1,60 @@
-function [ obrazkyRT_2D,obrazkyRT_3D,obrazkyIC_2D,obrazkyIC_3D,Errors,Poradi ] = aedistCSV( filenames )
+function [ obrazkyRT_2D,obrazkyRT_3D,obrazkyIC_2D,obrazkyIC_3D,Errors,Poradi ] = aedistCSV( filenames,datafolder )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 close all;
+if ~exist('datafolder','var'), datafolder = [pwd '\']; end %muzu mit soubory bez cesty a uvest ji v novem parametru
+csvfilelast = ''; %na zacatku je prazdne
+%19.4.2018 -  pouziti ruznych konfiguracnich souboru pro ruzne lidi - lepsi kdyz jsou serazene podle csv file, pak se kazdy csv nacita jen jednou
+for f = 1:size(filenames,1)
+    csvfile = filenames{f,2};    
+    if strcmp(csvfile,csvfilelast)==0 %pokud jiny csv file nez v predchozim radku
+        
+        if f>1, fprintf('\n'); end %ukoncim radku
+        assert(exist(csvfile,'file')==2, 'pozadovany csv file nenalezen');
+        Poradi = import2D3Dporadi(csvfile); %soubor se vzorovym poradim podminek %'AEDist2D3D poradi.csv'
+        fprintf('%s:',csvfile);
+        csvfilelast = csvfile;
+        
+        iTrening = Poradi.zpetnavazba == 1; %indexy treningovych pokusu
+        Poradi(iTrening,:) = []; % i tady potrebuju smazat treningove pokusy
+        %bloky podle otazky
+        Podle = cell(3,1);
+        Podle{1} = contains(Poradi.podle,'cervena');
+        Podle{2} = contains(Poradi.podle,'vy');
+        Podle{3} = contains(Poradi.podle,'znacka');
 
-%TODO -  pouziti ruznych konfiguracnich souboru pro ruzne lidi - mozna je to zbytecne?
-Poradi = import2D3Dporadi('AEDist2D3D inverze poradi.csv'); %soubor se vzorovym poradim podminek %'AEDist2D3D poradi.csv'
-iTrening = Poradi.zpetnavazba == 1; %indexy treningovych pokusu
-Poradi(iTrening,:) = []; % i tady potrebuju smazat treningove pokusy
-%bloky podle otazky
-Podle = cell(3,1);
-Podle{1} = contains(Poradi.podle,'cervena');
-Podle{2} = contains(Poradi.podle,'vy');
-Podle{3} = contains(Poradi.podle,'znacka');
+        d2D3D = cell(2,1);
+        %bloky podle 2D3D pohledu
+        d2D3D{1} = Poradi.d2D3D == 2;
+        d2D3D{2} = Poradi.d2D3D == 3; 
 
-d2D3D = cell(2,1);
-%bloky podle 2D3D pohledu
-d2D3D{1} = Poradi.d2D3D == 2;
-d2D3D{2} = Poradi.d2D3D == 3; 
+        %bloky Allo podminek
+        [blokyallozac,iAllo] = zacatkyBloku(Poradi);
 
-%bloky Allo podminek
-[blokyallozac,iAllo] = zacatkyBloku(Poradi);
+        %zakladam tabulku, obrazky jako nazvy sloupcu
+        obrazky = Poradi.obrazek(iAllo);
+        for o = 1:numel(obrazky)
+            obrazky{o} = basename(obrazky{o}); %potrebuju odstranit pripony souboru .png
+        end
+    end
+    if f == 1 %pokud se jedna o prvni cyklus 
+        obrazkyRT_2D = array2table(nan(size(filenames,1),numel(obrazky)));
+        obrazkyRT_2D.Properties.VariableNames = obrazky;
+        obrazkyRT_3D = obrazkyRT_2D; %dve stejne prazdne tabulky na zacatku
+        obrazkyIC_2D = obrazkyRT_2D; %is correct - uspesnost u obrazku 2D
+        obrazkyIC_3D = obrazkyRT_2D; %is correct - uspesnost u obrazku 3D
+        RowNames = cell(size(filenames,1),1);
+        Errors = cell2table(cell(0,7), 'VariableNames', {'Subject','Condition', 'Dimension','Obrazek','Response','Missed','IsCorrect'}); %prazdna tabulka
+    end
 
-%zakladam tabulku, obrazky jako nazvy sloupcu
-obrazky = Poradi.obrazek(iAllo);
-for o = 1:numel(obrazky)
-    obrazky{o} = basename(obrazky{o}); %potrebuju odstranit pripony souboru .png
-end
-obrazkyRT_2D = array2table(nan(numel(filenames),numel(obrazky)));
-obrazkyRT_2D.Properties.VariableNames = obrazky;
-obrazkyRT_3D = obrazkyRT_2D; %dve stejne prazdne tabulky na zacatku
-obrazkyIC_2D = obrazkyRT_2D; %is correct - uspesnost u obrazku 2D
-obrazkyIC_3D = obrazkyRT_2D; %is correct - uspesnost u obrazku 3D
-RowNames = cell(numel(filenames),1);
-Errors = cell2table(cell(0,7), 'VariableNames', {'Subject','Condition', 'Dimension','Obrazek','Response','Missed','IsCorrect'}); %prazdna tabulka
-
-for f = 1:numel(filenames)
-    filename = filenames{f};
-    Sdata = importCSVfile(filename);
+    filename = filenames{f,1};    
+    Sdata = importCSVfile([datafolder filename]);
+    fprintf(' %s\n',filenames{f,1});
     [~,fname,~] = fileparts(filename);
     RowNames{f} = fname;
     Sdata(iTrening,:) = []; %smazu data z treningu    
     DataBloky = zeros(length(Podle),length(d2D3D),ceil(height(Sdata)/6)); % to musi byt 64 jinak nekde chyba
+    
     for p= 1:size(DataBloky,1) %cyklus podminky - Allo, Ego, Cervena
         for d= 1:size(DataBloky,2) %cyklus 2D vs 3D
             DataBloky(p,d,:) = Sdata.RTms(Podle{p} & d2D3D{d});
@@ -76,15 +90,23 @@ for f = 1:numel(filenames)
             end
         end        
     end
-    prumery = mean(DataBloky,3);
-    stderr = sem(DataBloky,3);
-    figure('name',[ basename(filename) 'prumery']);
-    bar(prumery);
-    hold on;
-    errorbar(prumery,stderr);
-    legend({'2D','3D'});      
+    
+    if filenames{f,3} > 0 %pokud se ma zobrazi obrazek
+        prumery = mean(DataBloky,3);
+        stderr = sem(DataBloky,3);
+        fh = figure('name',[ basename(filename) 'prumery']);
+        set(fh, 'Visible', 'off');
+        bar(prumery);
+        hold on;
+        errorbar(prumery,stderr);
+        legend({'2D','3D'},'Location','northwest');  
+        title(strrep(basename(filename),'_','\_'));
+        xticklabels({'cervena','vy','znacka'})
+        saveas(gcf,[ datafolder basename(filename) '.png'])
+        close(fh);
+    end
 end
-
+fprintf('\n'); %dalsi radka
 %obrazek Allo - jednotlivci
 % figure('name','allo times')
 % subplot(1,2,1)
@@ -94,19 +116,19 @@ end
 % plot(obrazkyRT_3D{:,:}');
 % title('3D');
 
-M2 = mean(obrazkyRT_2D{:,:},1); %prumer sloupcu - pres subjekty
+M2 = nanmean(obrazkyRT_2D{:,:},1); %prumer sloupcu - pres subjekty
 M2err = sem(obrazkyRT_2D{:,:},1);
 %[M2,M2i]=sort(M2,'descend');
 %obrazkyRT_2D = obrazkyRT_2D(:,M2i); %seradim sloupce v tabulce podle prumeru pres subjekty
 
-M3 = mean(obrazkyRT_3D{:,:},1); %prumer sloupcu - pres subjekty
+M3 = nanmean(obrazkyRT_3D{:,:},1); %prumer sloupcu - pres subjekty
 M3err = sem(obrazkyRT_3D{:,:},1); %prumer sloupcu - pres subjekty
 %[M3,M3i]=sort(M3,'descend');
 %obrazkyRT_3D = obrazkyRT_3D(:,M3i); %seradim sloupce v tabulce podle prumeru pres subjekty
 
-IC_2D = mean(obrazkyIC_2D{:,:},1); %prumer sloupcu - pres subjekty
+IC_2D = nanmean(obrazkyIC_2D{:,:},1); %prumer sloupcu - pres subjekty
 ICerr_2D = sem(obrazkyIC_2D{:,:},1);
-IC_3D = mean(obrazkyIC_3D{:,:},1); %prumer sloupcu - pres subjekty
+IC_3D = nanmean(obrazkyIC_3D{:,:},1); %prumer sloupcu - pres subjekty
 ICerr_3D = sem(obrazkyIC_3D{:,:},1);
 % IC = nanmean(cat(1,IC_2D,IC_3D),1); %prumer pro 2D i 3D obrazky
 % ICerr = nanmean(cat(1,ICerr_2D,ICerr_3D),1);
@@ -147,17 +169,16 @@ obrazkyIC_2D.Properties.RowNames = RowNames;
 obrazkyIC_3D.Properties.RowNames = RowNames;
 
 %zapisu vystupni tabulky do excelu
-writetable(obrazkyRT_2D,'obrazkyRT_2D.xls','WriteRowNames',true);
-writetable(obrazkyRT_3D,'obrazkyRT_3D.xls','WriteRowNames',true);
-writetable(obrazkyIC_2D,'obrazkyIC_2D.xls','WriteRowNames',true);
-writetable(obrazkyIC_3D,'obrazkyIC_3D.xls','WriteRowNames',true);
-writetable(Errors,'Errors.xls','WriteRowNames',true);
+writetable(obrazkyRT_2D,[datafolder 'obrazkyRT_2D.xls'],'WriteRowNames',true);
+writetable(obrazkyRT_3D,[datafolder 'obrazkyRT_3D.xls'],'WriteRowNames',true);
+writetable(obrazkyIC_2D,[datafolder 'obrazkyIC_2D.xls'],'WriteRowNames',true);
+writetable(obrazkyIC_3D,[datafolder 'obrazkyIC_3D.xls'],'WriteRowNames',true);
+writetable(Errors,[datafolder 'Errors.xls'],'WriteRowNames',true);
 end
 
 function y = sem(x,dim)
 if ~exist('dim','var'), dim = 2; end
-
-y = std(x,0,dim) / sqrt(size(x,dim));
+y = nanstd(x,0,dim) / sqrt(size(x,dim));
 end
 function n= basename(filename)
     [~,n,~] = fileparts(filename);
