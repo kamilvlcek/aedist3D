a5070ffaf9aca4b257c15ec6ccc6f573ef38e495
kamilvlcek <kamil@biomed.cas.cz>
2019-06-19 14:07:36 +0200

    AEdist3D - erspimgT - spocitani postupne nekolika frekv pasem
    - cyklus pres freqs, kazdy graf je v jednom subplotu.
    - v grafech je zvyraznena signifikance pomoci cervenych hvezdicek na care signifikance
    - na konec vystupniho souboru se napise + pokud je nem neco signif
    + podarilo se mi i popsat spravne frekvence na ose Y
    + emf obrazky do jineho adresare
    + vypisy na obrazovku o tom co se deje

--- erspimgT.m
+++ erspimgT.m
@@ -2,66 +2,84 @@ function erspimgT(erspdata,ersptimes,erspfreqs,STUDY,conditions,channel)
 
 %potrebuje erspdata a ersptimes a erspfreqs
 %erspdata - 3x1 cell, matrix 83x106x1x21  freq x time x ch x subjects
-%pasma = {'alfa',[8 12]; 'beta' [12 30]};
-freqs = [8 13]; %alfa        
-ifreqs = find( (erspfreqs >= freqs(1)) & (erspfreqs <= freqs(2)) );  
-erspdataNorm = zeros(numel(ifreqs),size(erspdata{1},2),size(erspdata{1},4),numel(erspdata)); %normalizova kopie erspdata
-    %freq x time x subjects x conditions
-    %do jedne matrix, conditions maji vsechny stejne rozmery
-for f = 1:numel(ifreqs)            
-    meanf = zeros(size(erspdata{1},2),size(erspdata{1},4),numel(erspdata));  % time x subjects x condidions, tam ulozim data pro tuto frekvenci
-    
-    for cond = 1:numel(erspdata) %conditions
-        meanf(:,:,cond) = squeeze(erspdata{cond}(ifreqs(f),:,1,:)); %data teto condition        
-    end
-    %chyba - potrebuju delit vsema condition najednou ale pak nemuzu pouzit erspdata{c}
-    erspdataNorm(f,:,:,:) = meanf(:,:,:); % ./ mean2(meanf); %delim prumerem 
-end        
-erspmean = squeeze(mean(erspdataNorm,1)); %time x subjects x conditions - prumery pro pasmo   
-pp = anovafdr(erspmean); %anova s fdr korekci
 
-fh = figure('Name','Pasmo alfa');
-set(fh, 'Position',  [1 1 1000 800]); % velikost obrazku je z nejakeho duvodu relativni vzhledem k monitoru
+freqs = {'alfa',[8 13],2; 'beta', [14 30],3; 'theta', [4 7.5],5; 'lowgamma', [31 50],8;'highgamma',[51 100],9}; % nazvy a pasma frekvenci, + cislo subplotu        
+fh = figure('Name','Pasma grafy');
+set(fh, 'Position',  [1 1 1200 600]); % velikost obrazku je z nejakeho duvodu relativni vzhledem k monitoru
 hue = 0.8;
 colorskat = {[0 0 0],[0 1 0],[1 0 0],[0 0 1]; [hue hue hue],[hue 1 hue],[1 hue hue],[hue hue 1]}; % prvni radka - prumery, druha radka errorbars = svetlejsi
+fprintf('Pasma ');  
+signif = '';
 
-subplot(3,2,[2, 4, 6]); %vpravo obrazek pro vsechny podminky - prumery za pasma
-yyaxis left
-plotsh = zeros(1,size(erspmean,3)); %handle na ploty, jen ty chci do legendy
-for cond = 1:size(erspmean,3) %cyklus pro jednotlive podminky - conditions
-    colorkatk = [colorskat{1,cond+1} ; colorskat{2,cond+1}]; %dve barvy, na caru a stderr plochu kolem
-    M = mean(erspmean(:,:,cond),2); %prumer pres subjekty 
-    E = std(erspmean(:,:,cond),[],2)/sqrt(size(erspmean,2)); %std err of mean / std(subjects)/pocet(subjects)
-    %plot(ersptimes,M);    
-    plotband(ersptimes, M, E,colorkatk(2,:)); %nejlepsi, je pruhledny, ale nejde kopirovat do corelu
-    %ciplot(M+E, M-E, T, colorkatk(2,:)); %funguje dobre pri kopii do corelu, ulozim handle na barevny pas
+for ff = 1:size(freqs,1)
+    fprintf('%s ... ',freqs{ff,1});
+    freqband = freqs{ff,2};   
+    ifreqs = find( (erspfreqs >= freqband(1)) & (erspfreqs <= freqband(2)) );  
+    erspdataNorm = zeros(numel(ifreqs),size(erspdata{1},2),size(erspdata{1},4),numel(erspdata)); %normalizova kopie erspdata
+        %freq x time x subjects x conditions
+        %do jedne matrix, conditions maji vsechny stejne rozmery
+    for f = 1:numel(ifreqs)            
+        meanf = zeros(size(erspdata{1},2),size(erspdata{1},4),numel(erspdata));  % time x subjects x condidions, tam ulozim data pro tuto frekvenci
+
+        for cond = 1:numel(erspdata) %conditions
+            meanf(:,:,cond) = squeeze(erspdata{cond}(ifreqs(f),:,1,:)); %data teto condition        
+        end
+        %chyba - potrebuju delit vsema condition najednou ale pak nemuzu pouzit erspdata{c}
+        erspdataNorm(f,:,:,:) = meanf(:,:,:); % ./ mean2(meanf); %delim prumerem 
+    end        
+    erspmean = squeeze(mean(erspdataNorm,1)); %time x subjects x conditions - prumery pro pasmo   
+    pp = anovafdr(erspmean); %anova s fdr korekci
+    if(min(pp)<0.05), signif = '+'; end
+    isignif = pp<0.05;
+    
+    subplot(3,3,freqs{ff,3}); %vpravo obrazek pro vsechny podminky - prumery za pasma
+    yyaxis left
+    plotsh = zeros(1,size(erspmean,3)); %handle na ploty, jen ty chci do legendy
+    for cond = 1:size(erspmean,3) %cyklus pro jednotlive podminky - conditions
+        colorkatk = [colorskat{1,cond+1} ; colorskat{2,cond+1}]; %dve barvy, na caru a stderr plochu kolem
+        M = mean(erspmean(:,:,cond),2); %prumer pres subjekty 
+        E = std(erspmean(:,:,cond),[],2)/sqrt(size(erspmean,2)); %std err of mean / std(subjects)/pocet(subjects)
+        %plot(ersptimes,M);    
+        plotband(ersptimes, M, E,colorkatk(2,:)); %nejlepsi, je pruhledny, ale nejde kopirovat do corelu
+        %ciplot(M+E, M-E, T, colorkatk(2,:)); %funguje dobre pri kopii do corelu, ulozim handle na barevny pas
+        hold on;
+        plotsh(cond) = plot(ersptimes,M,'-','LineWidth',2,'Color',colorkatk(1,:));  %prumerna odpoved,  ulozim si handle na krivku      
+    end
+    legend(plotsh,conditions); %nazvy podminek
+    yyaxis right
+    plot(ersptimes,pp,'-'); %cara signifikance
     hold on;
-    plotsh(cond) = plot(ersptimes,M,'-','LineWidth',2,'Color',colorkatk(1,:));  %prumerna odpoved,  ulozim si handle na krivku      
+    plot(ersptimes(isignif),pp(isignif),'*r','MarkerSize',2); % zvyraznena signif
+    ylim([0 1])
+    yticks(0:0.1:1)
+    title(freqs{ff,1}); % nazev frekvencniho pasma
 end
-legend(plotsh,conditions); %nazvy podminek
-yyaxis right
-plot(ersptimes,pp,'-');
-ylim([0 1])
 
 for cond = 1:size(erspmean,3)
-    subplot(3,2,(cond-1)*2+1); %vlevo tri obrazky - cas x frekvence - pro kontrolu
+    subplot(3,3,(cond-1)*3+1); %vlevo tri obrazky - cas x frekvence - pro kontrolu
     D = mean(erspdata{cond},4);%mean over subjects
     imagesc(ersptimes,erspfreqs,D); %vykresluju jen alfa
-    %porad mi nefunguje popis osy Y aby sedel
-    %set(gca, 'YTick',erspfreqs(1):10:erspfreqs(end), 'YTickLabel', round(erspfreqs(1:10:end))) % 20 ticks
-    %axis ij;
-    set(gca,'YDir','normal') 
+     
+    frex = [4 8 14 31 51]; %frekvence ktere chci na y ose zobrazit
+    ifrex = zeros(1,numel(frex)); 
+    for f = 1:numel(frex)
+        ifrex(f) = find(erspfreqs>=frex(f),1); 
+    end
+    l = linspace(round(min(erspfreqs)),round(max(erspfreqs)),numel(erspfreqs)); %takhle imagesc zobrazuje frekvence defaultne, jako by linearni skala mezi min a max
+    set(gca, 'YTick',l(ifrex), 'YTickLabel', frex) % na te linearni skale linspace zobrazim odpovidajici nelinearni hodnoty
+    set(gca,'YDir','normal') %otocim osu y
     ylabel('freq');
     xlabel('time [ms]');
     title(conditions{cond});
     colorbar;
 end 
-filename = [STUDY.filepath '\\figures_export\\' STUDY.name '_' 'Alfa' '_' cell2str(conditions,1) '_' channel];
+fprintf(' printing figures ... ');
+filename = [STUDY.filepath '\\figures_export\\' STUDY.name '_' 'Pasma' '_' cell2str(conditions,1) '_' channel signif];
 print(fh,filename,'-djpeg');
-filename = [STUDY.filepath '\\figures_export_emf\\' STUDY.name '_' 'Alfa' '_' cell2str(conditions,1) '_' channel];
+filename = [STUDY.filepath '\\figures_export_emf\\' STUDY.name '_' 'Pasma' '_' cell2str(conditions,1) '_' channel signif];
 print(fh,filename,'-dmeta');
 close(fh); %zavre aktualni obrazek
-
+fprintf(' OK\n');
 end %function 
 
 function pp = anovafdr(erspmean)
