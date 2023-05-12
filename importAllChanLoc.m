8d6897ee538987b80fca4cb95a9628564953fafe
Sofiia <sofi.moraresku@gmail.com>
2023-05-12 14:31:21 +0200

    scripts for importing channel location files from Krios:
    	- function importChanLocCSV.m - reads channel location data from csv file (already transformed by Myrousz's Python scripts) and adapts it to the format suitable 	  for EEGLAB, saves in txt file
    
    	- script importAllChanLoc.m does it for all cvs files in the folder for all subjects

---
+++ importAllChanLoc.m
@@ -0,0 +1,11 @@
+% importAllChanLoc adapts csv channel locations files to the correct format in eeglab for all subjects
+
+cd 'E:\CIIRK\new_data\Krios channel loc\from_Myrousz'
+files=dir('*.csv');   %  all csv files in this directory 
+
+% For each subject
+for s=1:length(files)
+    chanLocFilename = files(s).name; % name of raw eeg file
+    subject = importChanLocCSV(chanLocFilename);
+    disp(['csv file for subject ' chanLocFilename ' succesfully converted to txt'])
+end
